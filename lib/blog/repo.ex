5ee83895ba1a53c8dcf95333e32b65247e628836
pochi <pochi.black@gmail.com>
2014-10-21 19:22:29 +0900

    Output log when execute query

--- lib/blog/repo.ex
+++ lib/blog/repo.ex
@@ -1,5 +1,6 @@
 defmodule Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
+  require Logger
 
   def conf do
     parse_url "ecto://pochi:pochi@localhost/phoenix_blog"
@@ -7,5 +8,11 @@ defmodule Repo do
 
   def priv do
     app_dir(:blog, "priv/repo")
+  end
+
+  def log({:query, sql}, fun) do
+    {time, result} = :timer.tc(fun)
+    Logger.info "[QUERY]" <> sql
+    result
   end
 end
